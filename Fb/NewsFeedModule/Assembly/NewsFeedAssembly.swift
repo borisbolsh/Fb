00ae16c525b6928c96c7f72c19c34518a6ac6f64
Boris Bo <bolsh.bo@yandex.ru>
2022-06-10 08:51:37 +0300

    update

--- Fb/NewsFeedModule/Assembly/NewsFeedAssembly.swift
+++ Fb/NewsFeedModule/Assembly/NewsFeedAssembly.swift
@@ -6,8 +6,7 @@ final class NewsFeedAssembly {
 		let presenter = NewsFeedPresenter(view: viewController)
 		let interactor = NewsFeedInteractor(presenter: presenter)
 		let router = NewsFeedRouter()
-
-
+		
 		viewController.presenter = presenter
 		presenter.interactor = interactor
 		presenter.router = router
